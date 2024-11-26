9d46c163580b3d76577a6bc44c1cf70bde1ef91c
abigailthomas <agthomas084@gmail.com>
2024-11-26 11:34:16 -0500

    Update functions.js
    Get coords from geocoding api

--- functions.js
+++ functions.js
@@ -1,1 +1,13 @@
-console.log("hi");
+function __init__() {
+    getCoords();
+}
+
+async function getCoords() {
+    let city = 'Asheville+NC';
+    let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyCandeVGmVHVrQS03jzYu5y2h0UpA95KoQ`);
+    data = await response.json();
+    console.log(data.results[0].geometry.location);
+    return data.results[0].geometry.location
+}
+
+__init__();
