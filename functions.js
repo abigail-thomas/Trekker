b41fd24b02bbf90b2b1e8fdf815587f6a84f9978
abigailthomas <agthomas084@gmail.com>
2024-11-26 11:42:22 -0500

    Update functions.js
    Return lat and long for places api

--- functions.js
+++ functions.js
@@ -1,13 +1,17 @@
-function __init__() {
-    getCoords();
+async function __init__() {
+    let {lat, lng} = await getCoords();
+    console.log(lat, lng);
 }
 
 async function getCoords() {
     let city = 'Asheville+NC';
     let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyCandeVGmVHVrQS03jzYu5y2h0UpA95KoQ`);
     data = await response.json();
-    console.log(data.results[0].geometry.location);
-    return data.results[0].geometry.location
+    //console.log(data.results[0].geometry.location);
+    return {
+        lat: data.results[0].geometry.location.lat,
+        lng: data.results[0].geometry.location.lng
+    };
 }
 
 __init__();
