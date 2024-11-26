7650c462841d3d123ef58dca92e73c2db412d30e
abigailthomas <agthomas084@gmail.com>
2024-11-26 12:30:09 -0500

    Change api
    Switch from Google api to Geocode Maps api

--- functions.js
+++ functions.js
@@ -4,14 +4,20 @@ async function __init__() {
 }
 
 async function getCoords() {
-    let city = 'Asheville+NC';
-    let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyCandeVGmVHVrQS03jzYu5y2h0UpA95KoQ`);
+    let location = 'Orlando+FL';
+    // let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyCandeVGmVHVrQS03jzYu5y2h0UpA95KoQ`);
+    let response = await fetch(`https://geocode.maps.co/search?q=${location}&api_key=674602f427fbe194124983rcya9edbe`);
     data = await response.json();
     //console.log(data.results[0].geometry.location);
+    // console.log(data[0].lat);
     return {
-        lat: data.results[0].geometry.location.lat,
-        lng: data.results[0].geometry.location.lng
+        // lat: data.results[0].geometry.location.lat,
+        // lng: data.results[0].geometry.location.lng
+        lat: data[0].lat,
+        lng: data[0].lon
     };
 }
+
+
 
 __init__();
